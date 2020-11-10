3a0dea1e266f1700724b083586f35ecf2329ffad
ikuyo-194 <kitamoe.0822@gmail.com>
2020-11-10 12:48:42 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -3,6 +3,12 @@ function setup(){
   noFill();
   for(let i = 0; i < 10; i++){
     let x = i * 10 + 10;
+    if(i < 5){
+      storoke(0, 0, 255);
+    }
+    else {
+      storoke(255, 0, 0);
+    }
 ellipse(50,50,x,x);    // BLANK[1]
   }
 }
