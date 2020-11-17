43ab39acfdbaf14742422ebc9284bb8ca6edf75d
ikuyo-194 <kitamoe.0822@gmail.com>
2020-11-17 12:04:58 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -4,10 +4,10 @@ function setup(){
   for(let i = 0; i < 10; i++){
     let x = i * 10 + 10;
     if(i < 5){
-      stroke(0, 0, 255);
+      storoke(0, 0, 255);
     }
     else {
-      stroke(255, 0, 0);
+      storoke(255, 0, 0);
     }
 ellipse(50,50,x,x);    // BLANK[1]
   }
